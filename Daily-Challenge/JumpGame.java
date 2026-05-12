a71dca7c9ccc2fdd1f9d6aa09a48d8acb0206aad
Nitish yadav <ny0172011@gmail.com>
2026-05-12 08:42:31 +0530

    Jump game

---
+++ Daily-Challenge/JumpGame.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n=nums.length;
+        int dest=n-1;
+        for(int i=n-2;i>=0;i--){
+            if(i + nums[i] >= dest){
+                dest=i;
+            }
+        }
+        if(dest==0){
+            return true;
+        }
+        return false;
+    }
+}
