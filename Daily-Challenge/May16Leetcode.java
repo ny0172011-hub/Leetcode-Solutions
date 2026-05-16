359f97a3988c00f375a076244a055a27d874993f
Nitish yadav <ny0172011@gmail.com>
2026-05-16 18:57:55 +0530

    Added to solution

---
+++ Daily-Challenge/May16Leetcode.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int n=nums.length;
+        int low=0;int high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if( mid>0 && mid<n-1 &&nums[mid]<nums[mid+1] && nums[mid]<nums[mid-1]){
+                return nums[mid];
+            }
+            if(nums[mid]>nums[high]){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return nums[low];
+    }
+}
